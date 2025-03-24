697359c37033221687ada4eee277160af9600e2a
Bhoovan Kumar <bhoovan2310@gmail.com>
2025-03-24 21:19:07 +0530

    UC3 Done -> Added: Event on The BridgeLabz Logo Using Event Binder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,18 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
   title = 'MyAngular';
   imageURL = '../assets/BL_logo_square_jpg.jpg';
+  url = 'https://www.bridgelabz.com/';
 
   ngOnInit(): void {
     console.log('Hello from ngOnInit');
     this.title = 'Hello from BridgeLabz.';
+  }
+
+  onClick($event: MouseEvent) {
+    console.log('Button was clicked!', $event);
+    window.open(this.url, '_blank');
   }
 }
